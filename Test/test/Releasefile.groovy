123c7c0293a210c359bccfed6d52dc6c6266112e
dhivya-digitalai <88141428+dhivya-digitalai@users.noreply.github.com>
2025-08-21 23:53:06 +1200

    Add files via upload

---
+++ Test/test/Releasefile.groovy
@@ -0,0 +1,24 @@
+// Exported from:        http://release.trial.digital.ai/#/templates/Folder1f0642c1df3c4f5e9d90065a4cee0225-Folder36984fc6789c477880899fee4dc5a9e6-Folder513f99420d2745a6b0d74cd89c40cf84-Folder39a7c2b5793146d085ca245441b674eb-Folderba48bb9195324000a471f509de1a1bf9-Release223ac60fc8184a14bdd64641b5c20564/code
+// Release version:      25.3.0-beta.724
+// Date created:         Thu Aug 21 11:46:35 GMT 2025
+
+xlr {
+  template('test') {
+    folder('dhivya.dhanasekaran.digital.ai/Demo/test/teest1/test2')
+    scheduledStartDate Date.parse("yyyy-MM-dd'T'HH:mm:ssZ", '2025-08-20T21:00:24+0000')
+    author 'dhivya.dhanasekaran.digital.ai'
+    phases {
+      phase('New Phase') {
+        tasks {
+          manual('New task') {
+            
+          }
+          manual('New task') {
+            
+          }
+        }
+      }
+    }
+    
+  }
+}
